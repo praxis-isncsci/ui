a4fc3edac5b3477c3508608f10ef3c658e24bd95
eddiemachete <elguardianenelsendero@hotmail.com>
2023-09-13 17:54:10 +0000

    + PraxisIsncsciTotals component

---
+++ src/web/praxisIsncsciTotals/praxisIsncsciTotals.stories.ts
@@ -0,0 +1,14 @@
+import {html} from 'lit';
+import type {Meta, StoryObj} from '@storybook/web-components';
+import './praxisIsncsciTotals';
+
+const meta = {
+  title: 'WebComponents/PraxisIsncsciTotals',
+  // tags: ['autodocs'],
+  render: () => html`<praxis-isncsci-totals></praxis-isncsci-totals>`,
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj;
+
+export const Primary: Story = {};
