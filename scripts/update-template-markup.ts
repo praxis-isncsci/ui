5040ac40eae5a0ad7c46f914cce66ecf1bec12c5
eddiemachete <elguardianenelsendero@hotmail.com>
2023-11-07 23:43:33 +0000

    Use template in markup

---
+++ scripts/update-template-markup.ts
@@ -0,0 +1,23 @@
+// 👇️ if using ES6 imports uncomment next line
+// import {readFile, writeFile, writeFileSync, promises as fsPromises} from 'fs';
+// import {getAppLayoutTemplate} from '../build/scripts/bundle.js';
+const {readFile, writeFile} = require('fs');
+const {getAppLayoutTemplate} = require('../build/scripts/appLayoutTemplate.js');
+
+console.log('replacing text in file...');
+
+readFile('./build/index.html', 'utf-8', function (err, contents) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const replaced = contents.replace(
+    /{{app-template}}/g,
+    getAppLayoutTemplate('', 'icons/'),
+  );
+
+  writeFile('./build/index.html', replaced, 'utf-8', function (err) {
+    console.log(err);
+  });
+});
