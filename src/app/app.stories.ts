3ab088b6732cf839c1d767c80c8af6c360bb51d5
eddiemachete <elguardianenelsendero@hotmail.com>
2023-11-08 05:36:03 +0000

    + Documentation

--- src/app/app.stories.ts
+++ src/app/app.stories.ts
@@ -2,6 +2,7 @@ import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 import {html} from 'lit-html';
 import type {Meta, StoryObj} from '@storybook/web-components';
 import {getAppLayoutTemplate} from '@web/praxisIsncsciAppLayout';
+import {AppStoreProvider} from '@app/providers';
 
 import '@app/webApp';
 import '@web/praxisIsncsciAppBar';
@@ -12,6 +13,8 @@ import '@web/praxisIsncsciInputLayout';
 import '@web/praxisIsncsciClassification';
 
 import 'assets/css/design-system.css';
+import {PraxisIsncsciWebApp} from '@app/webApp';
+import {appStore} from './store';
 
 const styles = html`
   <style>
@@ -20,21 +23,58 @@ const styles = html`
       height: 100vh;
       overflow: hidden;
     }
-
-    praxis-isncsci-web-app {
-      flex-grow: 1;
-    }
   </style>
 `;
+
+class webAppStory extends HTMLElement {
+  private template() {
+    return `
+      <style>
+        :host {
+          display: flex;
+          flex-direction: column;
+          flex-grow: 1;
+        }
+
+        ::slotted(praxis-isncsci-web-app) {
+          flex-grow: 1;
+        }
+      </style>
+      <slot></slot>
+    `;
+  }
+
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.innerHTML = this.template();
+  }
+
+  public connectedCallback() {
+    const webApp = document.querySelector(
+      'praxis-isncsci-web-app',
+    ) as PraxisIsncsciWebApp;
+
+    if (webApp) {
+      webApp.initialize(appStore, new AppStoreProvider(appStore));
+    }
+  }
+}
+window.customElements.define('web-app-story', webAppStory);
 
 const meta = {
   title: 'App/App',
   render: (args) =>
-    html`${styles}<praxis-isncsci-web-app
-        >${unsafeHTML(
-          getAppLayoutTemplate(args.classificationStyle, 'assets/icons'),
-        )}</praxis-isncsci-web-app
-      >`,
+    html`
+      ${styles}
+      <web-app-story>
+        <praxis-isncsci-web-app>
+          ${unsafeHTML(
+            getAppLayoutTemplate(args.classificationStyle, 'assets/icons'),
+          )}
+        </praxis-isncsci-web-app>
+      </web-app-story>
+    `,
 } satisfies Meta;
 
 export default meta;
