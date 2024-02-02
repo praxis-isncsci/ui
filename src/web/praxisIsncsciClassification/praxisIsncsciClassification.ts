8ecb3bb353216c3277d3626baa9dcc1beaf4cdee
eddiemachete <elguardianenelsendero@hotmail.com>
2024-02-02 21:13:27 +0000

    Updated acrylic and padding

--- src/web/praxisIsncsciClassification/praxisIsncsciClassification.ts
+++ src/web/praxisIsncsciClassification/praxisIsncsciClassification.ts
@@ -11,6 +11,7 @@ export class PraxisIsncsciClassification extends HTMLElement {
       <style>
         :host {
           backdrop-filter: var(--backdrop-filter, blur(1.5rem));
+          -webkit-backdrop-filter: var(--backdrop-filter, blur(1.5rem));
           background-color: var(--background-color, rgba(255, 255, 255, 0.4));
           box-shadow: var(--box-shadow, inset 0 0 1rem rgba(255, 255, 255, 0.4), 0 2px 4px rgba(95, 24, 119, 0.1), 0 1px 6px rgba(95, 24, 119, 0.05));
           display: flex;
@@ -22,7 +23,7 @@ export class PraxisIsncsciClassification extends HTMLElement {
           flex-wrap: wrap;
           gap: var(--gap, 2.5rem);
           justify-content: center;
-          padding: var(--padding, 1rem);
+          padding: var(--padding, 1rem 1rem 2rem 1rem);
         }
       </style>
       <slot name="header"></slot>
