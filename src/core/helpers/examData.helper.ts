1416b2b64a48a6f726148f7ea1c79b4e7ef2245c
Anna Mironyuk <annamironyuk@Annas-Mac-mini.local>
2025-03-13 11:34:03 -0700

    updates

--- src/core/helpers/examData.helper.ts
+++ src/core/helpers/examData.helper.ts
@@ -1146,7 +1146,7 @@ const levelsInOrder = [
   'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8',
   'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12',
   'L1', 'L2', 'L3', 'L4', 'L5',
-  'S1', 'S2', 'S3'
+  'S1', 'S2', 'S3', 'S4_5'
 ];
 
 const getLevelName = (cellName: string): string => {
